8be4105defeb5ea0ada8708f856c4fc84f723f9f
AhmadMustafa01 <ahmadmustafa8601@gmail.com>
2023-11-04 14:47:40 +0500

    intial commit

---
+++ myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Initialize the datepicker for elements with the "datepicker" class
+    $('.datepicker').datepicker({
+        autoclose: true, // Automatically close the date picker when a date is selected
+    });
+});
